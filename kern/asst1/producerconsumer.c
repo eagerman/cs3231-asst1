2a8081b5dc1410d291de228b4c6ba8523065bf5b
Abdul Mohammed <abdu.mohd.1122@gmail.com>
2018-03-30 07:48:27 +1100

    Wrote up first solution/blueprint for part 3

--- kern/asst1/producerconsumer.c
+++ kern/asst1/producerconsumer.c
@@ -1,12 +1,22 @@
 /* This file will contain your solution. Modify it as you wish. */
 #include <types.h>
 #include "producerconsumer_driver.h"
+#include "opt-synchprobs.h"
+#include <lib.h>    /* for kprintf */
+#include <synch.h>  /* for P(), V(), sem_* */
+#include <thread.h> /* for thread_fork() */
+//#include <test.h>
 
 /* Declare any variables you need here to keep track of and
    synchronise your bounded. A sample declaration of a buffer is shown
    below. You can change this if you choose another implementation. */
 
 static struct pc_data buffer[BUFFER_SIZE];
+static int head;
+static int tail;
+static struct semaphore *empty; // starts out with BUFFER_SIZE
+static struct semaphore *full; // starts out with 0
+static struct semaphore *mutex; // self descriptive
 
 
 /* consumer_receive() is called by a consumer to request more data. It
@@ -17,11 +27,15 @@ struct pc_data consumer_receive(void)
 {
         struct pc_data thedata;
 
-        (void) buffer; /* remove this line when you start */
-
         /* FIXME: this data should come from your buffer, obviously... */
-        thedata.item1 = 1;
-        thedata.item2 = 2;
+        P(full);
+        P(mutex);
+        
+        thedata = buffer[head];
+        head    = (head + 1) % BUFFER_SIZE;
+        
+        V(mutex);
+        V(empty);
 
         return thedata;
 }
@@ -31,7 +45,14 @@ struct pc_data consumer_receive(void)
 
 void producer_send(struct pc_data item)
 {
-        (void) item; /* Remove this when you add your code */
+        P(empty);
+        P(mutex);
+        
+        tail = (tail + 1) % BUFFER_SIZE;
+        buffer[tail] = item;
+        
+        V(mutex);
+        V(full);
 }
 
 
@@ -42,10 +63,20 @@ void producer_send(struct pc_data item)
 
 void producerconsumer_startup(void)
 {
+        empty = sem_create("empty", BUFFER_SIZE);
+        full  = sem_create("full", 0);
+        mutex = sem_create("mutex", 1);
+        if (empty == NULL || full == NULL)
+                panic("producerconsumer_startup: can't allocate semaphores!\n");
+        head = 0;
+        tail = 0;
 }
 
 /* Perform any clean-up you need here */
 void producerconsumer_shutdown(void)
 {
+        sem_destroy(empty);
+        sem_destroy(full);
+        sem_destroy(mutex);
 }
 
